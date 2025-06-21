31421447b812962608d4cecd6da872ee7fcaaa06
Radhey235 <Prathamm139@gmail.com>
2025-06-21 08:39:22 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/notes')
 
+
 const app = express()
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -23,6 +24,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
